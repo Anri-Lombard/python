c7563cd343de315850c2e083f537ea1b7af319d7
Anri Lombard <anri.m.lombard@gmail.com>
2021-06-13 13:33:57 +0200

    Optimal solution

--- Codewars/June/13/divisor.py
+++ Codewars/June/13/divisor.py
@@ -17,3 +17,13 @@ def divisors(integer):
 
 
 divisors(13)
+
+"""
+Optimal code:
+
+def divisors(num):
+    l = [a for a in range(2,num) if num%a == 0]
+    if len(l) == 0:
+        return str(num) + " is prime"
+    return l
+"""
