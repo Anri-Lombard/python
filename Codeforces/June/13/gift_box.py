e04b9ee8c013d88e02c48e9b56178ac73fd4c0b9
Anri Lombard <anri.m.lombard@gmail.com>
2021-06-13 13:17:54 +0200

    Fall inlove with codeforces

---
+++ Codeforces/June/13/gift_box.py
@@ -0,0 +1,10 @@
+# Gift set
+
+"""
+To test which case will give the most gift sets:
+  Recursively call the set_possibilities function and test all
+  arrangements of sets, then output the largest one.
+"""
+
+# Woah, this is tricky. I like codeforces!
+
